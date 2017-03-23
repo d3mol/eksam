150cef0a4718cce33ab99a7902e2929d05876848
d3mol <markvaljak@gmail.com>
2017-03-23 12:52:27 +0200

    kaardi ja ukse check start

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 
 	
 	
-	
+	// polnud aega et seda korrektselt teha teha
 	$mysqli = new mysqli("localhost", "root", "root", "uksed");
 	$mysqli->set_charset("utf8");
 
@@ -32,9 +32,7 @@
 			array_push($results);*/
 	}
 
-	// check kas uks on olemas fn
-	
-	//kas kaart on olemas fn
+
 
 
 	
@@ -51,11 +49,32 @@
 		return $result;
 	}
 	
+
+	
 /*
 	function checkDoors(){
 		global $mysqli;
 		//"$stmt = $mysqli->prepare(SELECT door_nr FROM doors WHERE door_nr=?)"
 		
 		$stmt->bind_param();
-*/
+*/	
+	
+	
+	
+	
+	//et kaart oleks olemas
+	unction checkIfACardExists($card_nr){
+		global $mysqli;
+		$stmt = $mysqli->prepare("SELECT card_nr FROM cards WHERE card_nr=?");
+		$stmt->bind_param("i", $card_nr);
+		$stmt->bind_result($card_nr_db);
+		$stmt->execute();
+		$result = 0;
+		if($stmt->fetch()){
+			$result = $card_nr_db;
+		}
+		return $result;
+	}
+	
+	
 ?>
--- index.php
+++ index.php
@@ -2,6 +2,33 @@
 
 require_once("functions.php");
 
+	if(isset($_GET["search"])){
+		
+		$card_check = checkIfACardExists($_GET["search_card_nr"]);
+		$door_check = checkIfADoorExists($_GET["search_door_nr"]);
+	
+	if($card_check == 0){
+		// kontroll kas kaart on olemas
+		//$msg = "kaarti numbriga".$_GET["search_card_nr"]."pole";
+		
+	} elseif($door_check == 0) {
+		// kontroll kas uks on olemas
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+
+
 
 ?>
 
@@ -16,7 +43,12 @@ require_once("functions.php");
 		
 		<h1>Uksed</h1>
 		<h2>Kaardi õiguse kontroll</h2>
-			
+			<?php
+				// msg teade
+				/*if(isset($_GET["msg"])){
+					echo "{$_GET["msg"]}";
+				}*/
+			?>
 		<form action="index.php" method="get">
 			<input type="text" name="search_card_nr" placeholder="Kaardi number">
 			<input type="text" name="search_door_nr" placeholder="Ukse number">
