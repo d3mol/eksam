ed006d081da9c5bb4d73f45bcab4735f20a4316a
d3mol <markvaljak@gmail.com>
2017-03-23 12:27:26 +0200

    kaart ja uks, kontrolli uks

--- functions.php
+++ functions.php
@@ -15,13 +15,47 @@
 									INNER JOIN cards ON
 									card_doors.card_nr=cards.card_nr
 									WHERE card_doors.card_nr=? AND door_nr=?");
+		$stmt->bind_param("ii", $card_nr, $door_nr);
+		$stmt->bind_result($id_db, $exp_db);
+		$stmt->execute();
+		$results = array();
+		if($stmt->fetch()){
+			array_push($results, $id_db);
+			array_push($results, $exp_db);
+		} else {
+			array_push($results, 0);
+			array_push($results, 0);
+		}
+		return $results;
 		
-	}	
+		/*if($stmt->fetch()){
+			array_push($results);*/
+	}
+
+	// check kas uks on olemas fn
+	
+	//kas kaart on olemas fn
 
 
-
-
-
-
-
+	
+	function checkIfADoorExists($door_nr){
+		global $mysqli;
+		$stmt = $mysqli->prepare("SELECT door_nr FROM doors WHERE door_nr=?");
+		$stmt->bind_param("i", $door_nr);
+		$stmt->bind_result($door_nr_db);
+		$stmt->execute();
+		$result = 0;
+		if($stmt->fetch()){
+			$result = $door_nr_db;
+		}
+		return $result;
+	}
+	
+/*
+	function checkDoors(){
+		global $mysqli;
+		//"$stmt = $mysqli->prepare(SELECT door_nr FROM doors WHERE door_nr=?)"
+		
+		$stmt->bind_param();
+*/
 ?>
